54e16b442fe32e65a4ffa3dfa3cbd26f4e77d821
kumonuu <kumonuu@proton.me>
2025-10-11 13:02:21 +0100

    Add files via upload

--- gui/addressbook.py
+++ gui/addressbook.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import messagebox
+from tkinter.filedialog import *
 
 screen = tk.Tk()
 screen.title("Address Book")
@@ -7,7 +9,12 @@ screen.geometry("500x500")
 addresses = {}
 
 def open_book():
-    pass
+    file = askopenfile()
+    if file:
+        addresses = eval(file.read())
+        keys = addresses.keys()
+        for key in keys:
+            listbox.insert(tk.END,key)
 
 def update_add():
     key = nameInput.get()
@@ -17,15 +24,61 @@ def update_add():
     bdayVal = bdayInput.get()
     addresses[key] = [addressVal,mobileVal,emailVal,bdayVal]
     listbox.insert(tk.END,key)
+    clear_entries()
 
 def edit():
-    pass
+    index = listbox.curselection()
+    if index:
+        item = listbox.get(index)
+        nameInput.insert(0,item)
+        addressInput.insert(0,addresses[item][0])
+        mobileInput.insert(0,addresses[item][1])
+        emailInput.insert(0,addresses[item][2])
+        bdayInput.insert(0,addresses[item][3])
+    else:
+        messagebox.showerror("No Address Selected","You must select an address to edit it.")
 
 def delete():
-    pass
+    index = listbox.curselection()
+    if index:
+        item = listbox.get(index)
+        listbox.delete(index)
+        del addresses[item] 
+    else:
+        messagebox.showerror("No Address Selected","You must select an address to delete it.")
+        
 
 def save():
+    file = asksaveasfile(defaultextension='.txt')
+    if file:
+        print(addresses,file=file)
+    else:
+        messagebox.showwarning("File was not saved","Please make sure that the file exists.")
+
+
+def clear_entries():
+    nameInput.delete(0,tk.END)
+    addressInput.delete(0,tk.END)
+    mobileInput.delete(0,tk.END)
+    emailInput.delete(0,tk.END)
+    bdayInput.delete(0,tk.END)
+
+#reset function clears entries, deletes dicts
+def reset():
     pass
+
+def display(event):
+    screen2 = tk.Tk()
+    screen2.title("Display")
+    index = listbox.curselection()
+    if index:
+        item = listbox.get(index)
+        addressVal = addresses[item][0]
+        mobileVal = addresses[item][1]
+        emailVal = addresses[item][2]
+        bdayVal = addresses[item][3]
+        info = tk.Label(screen2,text=f"{item}\nAddress: {addressVal}\nMobile: {mobileVal}\nEmail: {emailVal}\nBirthday: {bdayVal}")
+        info.place(x=50,y=50)
 
 name = tk.Label(screen,text="Name:").place(x=270,y=80)
 address = tk.Label(screen,text="Address:").place(x=270,y=130)
@@ -52,6 +105,7 @@ saveBtn = tk.Button(screen,text="Save",width=50,command=save).place(x=60,y=380)
 
 listbox = tk.Listbox(screen,width=30,height=15)
 listbox.place(x=20,y=30)
+listbox.bind('<<ListboxSelect>>',display)
 
 
 screen.mainloop()
